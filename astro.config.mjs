3025242079ad32ab9c43fc660f1704900ef5ecb7
jhustyn <jhustynherwanap@gmail.com>
2023-12-03 07:39:20 +0700

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
